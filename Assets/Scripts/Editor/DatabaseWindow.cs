0893c9449c4116434df3fca3b8379ee06667e5a2
Enrico Ludwig <eludwig.work@gmail.com>
2019-10-15 14:01:16 +0200

    Day 1 - General project setup, Database Editor Window

---
+++ Assets/Scripts/Editor/DatabaseWindow.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class DatabaseWindow : EditorWindow
+{
+    private string dbPath;
+
+    [MenuItem("Unterricht/Database Connection")]
+    public static void OpenWindow()
+    {
+        var w = CreateInstance<DatabaseWindow>();
+        w.Show();
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Space(5);
+
+        // Database file selection
+        GUILayout.BeginHorizontal();
+        {
+            // Label and disabled text field for file path
+            GUILayout.Label("Database:", GUILayout.Width(70));
+            EditorGUI.BeginDisabledGroup(true);
+            {
+                GUILayout.TextField(dbPath);
+            }
+            EditorGUI.EndDisabledGroup();
+
+            // Open file dialog button
+            if (GUILayout.Button("...", GUILayout.Width(20), GUILayout.Height(13)))
+            {
+                string defaultDirectory = Application.dataPath;
+                string[] fileFilter = new string[] { "SQLite Database", "sqlite,db" };
+
+                dbPath = EditorUtility.OpenFilePanelWithFilters("Open Database", defaultDirectory, fileFilter);
+            }
+        }
+        GUILayout.EndHorizontal();
+
+        // Database connect button
+        GUILayout.BeginHorizontal();
+        {
+            EditorGUI.BeginDisabledGroup(string.IsNullOrEmpty(dbPath) || !File.Exists(dbPath));
+            {
+                if (GUILayout.Button("Connect"))
+                {
+                    
+                }
+            }
+            EditorGUI.EndDisabledGroup();
+        }
+        GUILayout.EndHorizontal();
+    }
+}
---
+++ Assets/Scripts/Mappings/PlayerData.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+
+[Table("PlayerData")]
+public class PlayerData
+{
+    [Column("ID")]
+    public int ID { get; set; }
+    
+    [Column("Name")]
+    public string Name { get; set; }
+
+    [Column("Health")]
+    public int Health { get; set; }
+
+    [Column("Mana")]
+    public int Mana { get; set; }
+
+    [Column("Credits")]
+    public int Credits { get; set; }
+
+    [Column("Class")]
+    public int Class { get; set; }
+}
