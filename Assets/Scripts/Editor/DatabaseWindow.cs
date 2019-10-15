a924c18643486fb73bfc18b776558c5495197fb6
Enrico Ludwig <eludwig.work@gmail.com>
2019-10-15 14:30:37 +0200

    Finished dis/connecting function

--- Assets/Scripts/Editor/DatabaseWindow.cs
+++ Assets/Scripts/Editor/DatabaseWindow.cs
@@ -3,9 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using SQLite;
 
 public class DatabaseWindow : EditorWindow
 {
+    /// <summary>
+    /// SQLite database connection
+    /// </summary>
+    public static SQLiteConnection Connection
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+    /// Returns <code>true</code> if a connection is established, otherwise <code>false</code>
+    /// </summary>
+    public static bool IsConnected
+    {
+        get
+        {
+            return Connection != null;
+        }
+    }
+
+    /// <summary>
+    /// Path to database file
+    /// </summary>
     private string dbPath;
 
     [MenuItem("Unterricht/Database Connection")]
@@ -13,6 +37,11 @@ public class DatabaseWindow : EditorWindow
     {
         var w = CreateInstance<DatabaseWindow>();
         w.Show();
+    }
+
+    private void OnEnable()
+    {
+        dbPath = EditorPrefs.GetString("DB_PATH");
     }
 
     private void OnGUI()
@@ -40,19 +69,48 @@ public class DatabaseWindow : EditorWindow
             }
         }
         GUILayout.EndHorizontal();
-
-        // Database connect button
+        
         GUILayout.BeginHorizontal();
         {
-            EditorGUI.BeginDisabledGroup(string.IsNullOrEmpty(dbPath) || !File.Exists(dbPath));
+            // Database connect button
+            EditorGUI.BeginDisabledGroup(string.IsNullOrEmpty(dbPath) || !File.Exists(dbPath) || IsConnected);
             {
                 if (GUILayout.Button("Connect"))
                 {
-                    
+                    Connect();
+                }
+            }
+            EditorGUI.EndDisabledGroup();
+
+            // Database disconnect button
+            EditorGUI.BeginDisabledGroup(!IsConnected);
+            {
+                if (GUILayout.Button("Disconnect"))
+                {
+                    Disconnect();
                 }
             }
             EditorGUI.EndDisabledGroup();
         }
         GUILayout.EndHorizontal();
+    }
+
+    private void Connect()
+    {
+        EditorPrefs.SetString("DB_PATH", dbPath);
+
+        SQLiteConnectionString connectionString = new SQLiteConnectionString(dbPath);
+        Connection = new SQLiteConnection(connectionString);
+
+        
+    }
+
+    private void Disconnect()
+    {
+        if (Connection == null)
+            return;
+
+        Connection.Close();
+        Connection = null;
     }
 }
